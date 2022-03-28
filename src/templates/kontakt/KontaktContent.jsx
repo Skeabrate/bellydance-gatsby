ff6013aed10b90c49817b0d73c9d490545f8a922
unknown <sebastianswiecz458@gmail.com>
2022-03-28 11:44:31 +0200

    Kontakt page without styles

--- src/templates/kontakt/KontaktContent.jsx
+++ src/templates/kontakt/KontaktContent.jsx
@@ -1,11 +1,61 @@
 import React from 'react';
 import Heading from 'components/Heading/Heading';
+import ContentWrapper from 'components/ContentWrapper/ContentWrapper';
+import Phone from 'assets/images/svg/telefon.svg';
+import { StaticImage } from 'gatsby-plugin-image';
+import Frame from 'components/Frame/Frame';
+
+import { StyledSection, StyledForm } from './KontaktContent.styles';
 
 const KontaktContent = () => {
     return (
-        <div>
+        <ContentWrapper>
             <Heading label="Kontakt" isMain />
-        </div>
+
+            <StyledSection>
+                <article>
+                    <header>
+                        <h2>Lorem ipsum dolor sit!</h2>
+                        <p>
+                            Nieustannie rozwijam swoją technikę taneczną,
+                            uczestnicząc w warsztatach prowadzonych przez
+                            wybitnych nauczycieli tańca orientalnego.
+                        </p>
+
+                        <p>
+                            <Phone /> Telefon:{' '}
+                            <a
+                                href="tel:'502 505 119"
+                                rel="noopener noreferrer"
+                            >
+                                502 505 119
+                            </a>{' '}
+                        </p>
+                    </header>
+
+                    <StyledForm>
+                        <input type="text" placeholder="Imię" />
+                        <input type="email" placeholder="Adres e-mail" />
+                        <textarea
+                            name="message"
+                            id="message"
+                            cols="30"
+                            rows="10"
+                            placeholder="Wiadomość"
+                        ></textarea>
+                    </StyledForm>
+                </article>
+
+                <Frame>
+                    <StaticImage
+                        src="../../assets/images/Pokazy/img7.jpg"
+                        alt="Agnieszka Świeczkowska - kontakt"
+                        placeholder="blurred"
+                        layout="fullWidth"
+                    />
+                </Frame>
+            </StyledSection>
+        </ContentWrapper>
     );
 };
 
--- src/templates/kontakt/KontaktContent.styles.jsx
+++ src/templates/kontakt/KontaktContent.styles.jsx
@@ -1,1 +1,5 @@
 import styled from 'styled-components';
+
+export const StyledSection = styled.section``;
+
+export const StyledForm = styled.form``;
