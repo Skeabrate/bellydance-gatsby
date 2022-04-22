df4f44e61a0a3540f03248544947595376d308ee
unknown <sebastianswiecz458@gmail.com>
2022-04-22 14:38:45 +0200

    try fix paralax mobile bugg v2

--- src/templates/ParalaxWrapper.jsx
+++ src/templates/ParalaxWrapper.jsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   position: sticky;
   background-color: white;
-  zindex: 1;
+  zindex: 10;
 `;
 
 const ParalaxWrapper = ({ children }) => {
