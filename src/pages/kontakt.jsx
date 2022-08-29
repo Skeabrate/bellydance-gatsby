218f488c9aded8fd48abac5564e8a1c6e09eeea3
Sebastian Świeczkowski <sebastianswiecz458@gmail.com>
2022-08-30 00:40:27 +0200

    formspree env var

--- src/pages/kontakt.jsx
+++ src/pages/kontakt.jsx
@@ -12,7 +12,7 @@ import { useForm } from '@formspree/react';
 import { StyledSection, StyledForm, StyledFormResponse } from 'assets/styles/pages/kontakt.styles';
 
 const Kontakt = () => {
-  const [state, handleSubmit] = useForm(process.env.FORMSPREE_API);
+  const [state, handleSubmit] = useForm(process.env.GATSBY_FORMSPREE_API);
 
   const { placeholderImage } = useKontaktQuery();
   const formRef = useRef(null);
