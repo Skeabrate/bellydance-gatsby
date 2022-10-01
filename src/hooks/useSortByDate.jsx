c5e6368467675bd7b35bfe34402725b29f5eada6
Sebastian Świeczkowski <sebastianswiecz458@gmail.com>
2022-10-02 00:12:32 +0200

    Fixed sorting by date

--- src/hooks/useSortByDate.jsx
+++ src/hooks/useSortByDate.jsx
@@ -4,11 +4,16 @@ export const useSortByDate = (data) => {
   const [sortByDate, setSortByDate] = useState(true);
 
   (function () {
-    data?.allDatoCmsPost.edges.sort((a, b) =>
-      sortByDate
-        ? Date.parse(b.node.meta.firstPublishedAt) - Date.parse(a.node.meta.firstPublishedAt)
-        : Date.parse(a.node.meta.firstPublishedAt) - Date.parse(b.node.meta.firstPublishedAt)
-    );
+    data?.allDatoCmsPost.edges.sort((a, b) => {
+      const aDate = a.node.date || a.node.meta.firstPublishedAt;
+      const bDate = b.node.date || b.node.meta.firstPublishedAt;
+
+      if (sortByDate) {
+        return Date.parse(bDate) - Date.parse(aDate);
+      } else {
+        return Date.parse(aDate) - Date.parse(bDate);
+      }
+    });
   })();
 
   return { setSortByDate };
