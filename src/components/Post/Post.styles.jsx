1a9dd0e9a469304260a47bc681f2df85c400a90b
Sebastian Świeczkowski <sebastianswiecz458@gmail.com>
2022-10-02 00:34:48 +0200

    Fixed overflowed post img

--- src/components/Post/Post.styles.jsx
+++ src/components/Post/Post.styles.jsx
@@ -14,6 +14,7 @@ export const MediaContainer = styled.div`
   background-color: ${({ theme }) => theme.colors.sand};
   position: relative;
   max-height: 300px;
+  overflow: hidden;
 
   img {
     max-height: 300px;
